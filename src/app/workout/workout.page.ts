6f6e7d4e9ac3a65ddbb940173a35084d90426016
abhilash-vadlamudi <abhilash.vadlamudi@hotmail.com>
2019-01-09 16:51:10 +0530

    workout page update

--- src/app/workout/workout.page.ts
+++ src/app/workout/workout.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GoogleDriveService } from 'src/app/services/google-drive.service';
 import { SheetTabsTitleConst } from '../constants/sheet.constant';
-import { DriveRequestModel } from '../models/drive-postdata.model';
 import { AppService} from '../services/app.service';
 
 @Component({
@@ -17,22 +16,25 @@ export class WorkoutPage implements OnInit {
 
   ngOnInit() {
     // const workData = this.googleDriveService.getLocalSheetTabData(SheetTabsTitleConst.WORKOUT);
-    // console.log(workData);
-    const url = this.appservice.getParsedGetDataUrl(this.googleDriveService.getSheetId(), SheetTabsTitleConst.WORKOUT);
-    console.log(url);
+    // console.log(workData.data);
+    const Url = this.appservice.getParsedGetDataUrl(this.googleDriveService.getSheetId(), SheetTabsTitleConst.WORKOUT);
+    // console.log(Url);
     let data: string;
+    function abhi(url) {
     fetch(url).then(function(response) {return response.json(); }).then(function(myJson) {data = myJson;
-      const value = data['values'] ;
-      // console.log(data);
-      console.log(value);
-      const list: string[] = [];
-      value.forEach(element => {
-      list.push(element.value);
-       });
-      console.log(list);
-     });
-
+    const values = data['values'] ;
+    // console.log(value);
+    let list: string[] = [];
+    // let  title: string[] = [];
+    // title = values[0];
+    // console.log(title);
+    values.forEach(element => {
+      for ( let i = 0; i < 10; i++) {
+      if (new Date().toLocaleDateString() === element[i]) { list = element; } }
+      });
+    console.log(list);
+    });
   }
-
-
+  abhi(Url);
+}
 }
