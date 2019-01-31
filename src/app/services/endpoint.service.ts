6b7b9be1758707ffd7b36d27fef56b0144e49f4d
abhilash-vadlamudi <abhilash.vadlamudi@hotmail.com>
2019-01-31 09:33:02 -0800

    removed api

--- src/app/services/endpoint.service.ts
+++ src/app/services/endpoint.service.ts
@@ -7,6 +7,6 @@ export class EndpointService {
   public readonly googleApi = 'https://sheets.googleapis.com/v4/spreadsheets/';
   public readonly apiKey = 'AIzaSyDhrD_jc40DRSgxzwpw_BVJ80B8ZgzlAso';
   public readonly sendgridApi = 'https://api.sendgrid.com/v3/mail/send';
-  public readonly sgApiKey = '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';
+  public readonly sgApiKey = '';
 }
 
