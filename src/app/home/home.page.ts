90b0ad6c6133771b32f6b18b2106203f97415e60
abhilash-vadlamudi <abhilash.vadlamudi@hotmail.com>
2019-02-04 15:14:38 -0800

    Undo changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,28 +19,28 @@ export class HomePage {
   ionViewWillEnter() {
 
     this.menuCtrl.enable(true);
-    // this.checkForInitialSetup();
+    this.checkForInitialSetup();
   }
-  // checkForInitialSetup() {
+  checkForInitialSetup() {
 
-  //   this.googleDriveService.getAllSheetData(this.googleDriveService.getSheetId()).subscribe(
-  //     sheetData => {
-  //       this.googleDriveService.saveAllSheetData(sheetData['valueRanges']);
-  //       // console.log('this.isProfileSetupComplete()', this.googleDriveService.isProfileSetupComplete());
-  //       if (this.googleDriveService.isProfileSetupComplete() === false) {console.log('Signup form is not updated');
-  //       this.router.navigateByUrl('/signupform');
-  //     } else if (this.googleDriveService.isGoalSetupComplete() === false) {console.log('Goals setup is not updated');
-  //     this.router.navigateByUrl('/goals');
-  //     } else if (this.googleDriveService.isMedicalSetupComplete() === false) {console.log('Medical history is not updated');
-  //     this.router.navigateByUrl('/medicalhistory');
-  //     } else {
-  //     this.router.navigateByUrl('/thankyou'); }
-  //     },
-  //     err => {
-  //       console.error(err);
-  //     }
-  //   );
+    this.googleDriveService.getAllSheetData(this.googleDriveService.getSheetId()).subscribe(
+      sheetData => {
+        this.googleDriveService.saveAllSheetData(sheetData['valueRanges']);
+        // console.log('this.isProfileSetupComplete()', this.googleDriveService.isProfileSetupComplete());
+        if (this.googleDriveService.isProfileSetupComplete() === false) {console.log('Signup form is not updated');
+        this.router.navigateByUrl('/signupform');
+      } else if (this.googleDriveService.isGoalSetupComplete() === false) {console.log('Goals setup is not updated');
+      this.router.navigateByUrl('/goals');
+      } else if (this.googleDriveService.isMedicalSetupComplete() === false) {console.log('Medical history is not updated');
+      this.router.navigateByUrl('/medicalhistory');
+      } else {
+      this.router.navigateByUrl('/thankyou'); }
+      },
+      err => {
+        console.error(err);
+      }
+    );
 
-  // }
+  }
 
 }
