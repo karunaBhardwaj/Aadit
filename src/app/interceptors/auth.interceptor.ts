8420af049e86ed7988f6ab913a5c986a17f25fef
abhilash-vadlamudi <abhilash.vadlamudi@hotmail.com>
2019-06-14 12:09:09 +0530

    login modified

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,21 +1,13 @@
-import { Injectable} from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { GoogleDriveService } from '../services/google-drive.service';
-import { AppService } from '../services/app.service';
-import { EndpointService } from '../services/endpoint.service';
-import * as firebase from 'firebase';
-import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-private user: firebase.User;
 
-    constructor(private googleDriveService: GoogleDriveService,
-      private appservice: AppService, private endpoint: EndpointService,  private http: HttpClient, private auth: AuthService) {
+    constructor(private googleDriveService: GoogleDriveService) {
     }
-
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         request = request.clone({
@@ -23,20 +15,7 @@ private user: firebase.User;
                 Authorization: `Bearer ${this.googleDriveService.getOauthToken()}`
             })
         });
-        request = request.clone(request);
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-              this.auth.refresh();
-              // return next.handle(request);
-            } else if (err.status === 500) {
-              err.statusText = 'Oops';
-              err.error.error.message = 'Something went wrong!';
-            }
-            const error = {
-              title: err.statusText || 'Oops!',
-              message: err.error.error.message || 'Something went wrong!'
-            };
-            return throwError(error);
-          }));
+
+        return next.handle(request);
     }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,6 @@ import { UserProfileModel, UserInfoModel, TokenModel } from '../models/user-info
 import { Router } from '@angular/router';
 import { DbqueryService } from './dbquery.service';
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
-import * as $ from 'jquery';
 import { AppService } from './app.service';
 import { firebaseConfig } from '../../config';
 import { LoadingController} from '@ionic/angular';
@@ -43,66 +42,18 @@ export class AuthService {
   get authenticated(): boolean {
     return this.user !== null;
   }
-  async refresh() {
-    const settings = {
-      'async': true,
-      'crossDomain': true,
-      'url': 'https://www.googleapis.com/oauth2/v4/token',
-      'method': 'POST',
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'cache-control': 'no-cache',
-      },
-      'data': {
-        'grant_type': 'refresh_token',
-        'client_id': firebaseConfig.firbase.client_id,
-        'client_secret': firebaseConfig.firbase.client_secret,
-        'refresh_token': JSON.parse(localStorage.getItem('userInfo')).token.refreshToken
-      }
-    };
-
-    await $.ajax(settings).done(function (response) {
-      console.log(response);
-      const data = JSON.parse(localStorage.getItem('userInfo'));
-        data['token']['oAuthToken'] = response.access_token;
-        localStorage.setItem('userInfo', JSON.stringify(data));
-        console.log(response.access_token);
-    });
-  }
-
-  async generateToken(code) {
-    const settings = {
-      'async': true,
-      'crossDomain': true,
-      'url': 'https://www.googleapis.com/oauth2/v4/token',
-      'method': 'POST',
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'cache-control': 'no-cache',
-      },
-      'data': {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'client_id': firebaseConfig.firbase.client_id,
-        'client_secret': firebaseConfig.firbase.client_secret,
-        'redirect_uri': ''
-      }
-    };
-    let data;
-    await $.ajax(settings).done(function (response) {
-      console.log(response);
-      data = response;
-    });
-    this.userToken = await data;
-  }
 
   getEmail() {
     return this.user && this.user.email;
   }
-  signOut(): Promise<void> {
-    this.googlePlus.logout();
-    localStorage.clear();
-    return this.afAuth.auth.signOut();
+  signOut() {
+    this.googlePlus.logout()
+    .then(res => {
+      localStorage.clear();
+      this.router.navigate(['/login']);
+    }, err => {
+      console.log(err);
+    });
   }
   signInWithEmail(credentials) {
     console.log('Sign in with email');
@@ -120,39 +71,6 @@ export class AuthService {
 
         return this.signInHandler(createUserWithCredsResponse);
       });
-  }
-
-  async signInWithGoogle(): Promise<void> {
-    const loading = await this.loadingController.create({
-      message: 'Please wait...'
-    });
-    this.presentLoading(loading);
-
-  await this.googlePlus.login({
-            'scopes': 'profile https://www.googleapis.com/auth/spreadsheets',
-            'webClientId': firebaseConfig.firbase.client_id,
-            'offline': true
-  }).then(res => {
-    this.authToken = res.serverAuthCode;
-  }).catch(err => {
-    console.error(err);
-    loading.dismiss();
-    this.ErrorAlert(err);
-  });
-  await this.generateToken(this.authToken);
-    const credential = auth.GoogleAuthProvider.credential(this.userToken.id_token);
-
-    return this.afAuth.auth.signInAndRetrieveDataWithCredential(credential)
-    .then(res => {
-        console.log('signInWithGoogle', res);
-        this.signInHandler(res);
-        loading.dismiss();
-
-    }).catch(err => {
-      console.error(err);
-      loading.dismiss();
-      this.ErrorAlert(err);
-    });
   }
 
   async ErrorAlert(err) {
@@ -184,14 +102,48 @@ export class AuthService {
     alert.present();
   }
 
+  async presentLoading(loading) {
+    return await loading.present();
+  }
+
+  async signInWithGoogle(): Promise<void> {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+    this.presentLoading(loading);
+    await this.googlePlus.login({
+              'scopes': 'profile https://www.googleapis.com/auth/spreadsheets',
+              'webClientId': firebaseConfig.firbase.client_id,
+              'offline': true
+    }).then(res => {
+      console.log(res);
+      this.userToken = res.idToken;
+      this.authToken = res.accessToken;
+    }).catch(err => {
+      console.error(err);
+      loading.dismiss();
+      this.ErrorAlert(err);
+    });
+      const credential = auth.GoogleAuthProvider.credential(this.userToken);
+
+      return this.afAuth.auth.signInAndRetrieveDataWithCredential(credential)
+      .then(res => {
+          console.log('signInWithGoogle', res);
+          this.signInHandler(res);
+          loading.dismiss();
+      });
+    }
+
 
   public signInHandler(data): void {
+    console.log('this.signInHandler', data);
+
     this.afDb.database.ref('profile').orderByChild('userId').equalTo
     (data['additionalUserInfo']['profile']['email']).on('child_added', (snapshot) => {
       const sheetId: string = snapshot.child('sheetId')['node_']['value_'];
 
-      const userInfo = new UserInfoModel(new TokenModel(this.userToken.access_token,
-      this.userToken.refresh_token, sheetId), new UserProfileModel(data['additionalUserInfo']['profile']['email'],
+      const userInfo = new UserInfoModel(new TokenModel(this.authToken,
+      data['user']['refreshToken'], sheetId), new UserProfileModel(data['additionalUserInfo']['profile']['email'],
       data['additionalUserInfo']['profile']['family_name'], data['additionalUserInfo']['profile']['given_name'],
       data['additionalUserInfo']['profile']['name'], data['additionalUserInfo']['profile']['picture']));
 
@@ -204,10 +156,8 @@ export class AuthService {
       // this.router.navigate(['/home']);
       this.formstatusservice.checkmenustatus();
       this.formstatusservice.checkForInitialSetup();
-      });
-  }
-  async presentLoading(loading) {
-    return await loading.present();
+    });
+
   }
 
 }
