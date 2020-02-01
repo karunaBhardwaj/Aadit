c9007c1d1774b566995b8084805509edb9d9978c
Abhilash Vadlamudi <abhilash.v@BT-C02VN0S4G8WN.local>
2020-02-01 23:11:13 +0530

    Workout Page modifications

--- src/app/workout/workout.page.ts
+++ src/app/workout/workout.page.ts
@@ -8,7 +8,7 @@ import { SheetsService } from '../services/sheets.service';
   styleUrls: ['./workout.page.scss'],
 })
 export class WorkoutPage implements OnInit {
-  selectedOption = 'select';
+  selectedOption;
   weekDay: string;
   WorkoutData;
   strngthExData;
@@ -44,17 +44,14 @@ export class WorkoutPage implements OnInit {
         if (this.weekDay === 'Strength') {
           document.getElementById('Workout').style.display = 'none';
           document.getElementById('rest').style.display = 'none';
-          document.getElementById('select').style.display = 'none';
           document.getElementById('Strengthex').style.display = 'block';
-        } else if(this.weekDay === 'Rest') {
+        } else if (this.weekDay === 'Rest') {
           document.getElementById('Workout').style.display = 'none';
           document.getElementById('Strengthex').style.display = 'none';
-          document.getElementById('select').style.display = 'none'; 
           document.getElementById('rest').style.display = 'block';
         } else {
           document.getElementById('Strengthex').style.display = 'none';
           document.getElementById('rest').style.display = 'none';
-          document.getElementById('select').style.display = 'none';
           document.getElementById('Workout').style.display = 'block';
           this.Data = this.WorkoutData[this.weekDay];
           this.title = this.WorkoutData['Title'];
@@ -62,47 +59,37 @@ export class WorkoutPage implements OnInit {
       } else if (selectedOption === 'Long') {
         document.getElementById('Strengthex').style.display = 'none';
         document.getElementById('rest').style.display = 'none';
-        document.getElementById('select').style.display = 'none';
         document.getElementById('Workout').style.display = 'block';
         this.Data = this.WorkoutData['Long'];
         this.title = this.WorkoutData['Title'];
       } else if (selectedOption === 'Tempo') {
         document.getElementById('Strengthex').style.display = 'none';
         document.getElementById('rest').style.display = 'none';
-        document.getElementById('select').style.display = 'none';
         document.getElementById('Workout').style.display = 'block';
         this.title = this.WorkoutData['Title'];
         this.Data =  this.WorkoutData['Tempo'];
       } else if (selectedOption === 'Interval') {
         document.getElementById('Strengthex').style.display = 'none';
         document.getElementById('rest').style.display = 'none';
-        document.getElementById('select').style.display = 'none';
         document.getElementById('Workout').style.display = 'block';
         this.Data =  this.WorkoutData['Interval'];
         this.title = this.WorkoutData['Title'];
       } else if (selectedOption === 'Speed') {
         document.getElementById('Strengthex').style.display = 'none';
         document.getElementById('rest').style.display = 'none';
-        document.getElementById('select').style.display = 'none';
         document.getElementById('Workout').style.display = 'block';
         this.Data =  this.WorkoutData['Speed'];
         this.title = this.WorkoutData['Title'];
       } else if (selectedOption === 'Strengthex') {
         document.getElementById('Workout').style.display = 'none';
         document.getElementById('rest').style.display = 'none';
-        document.getElementById('select').style.display = 'none';
         document.getElementById('Strengthex').style.display = 'block';
-      } else if (selectedOption === 'select') {
-        document.getElementById('Workout').style.display = 'none';
-        document.getElementById('rest').style.display = 'none';
-        document.getElementById('Strengthex').style.display = 'none';
-        document.getElementById('select').style.display = 'block';
       }
     }
 
   async ngOnInit() {
+    this.selectedOption = 'Today';
     document.getElementById('Day').innerHTML = new Date().toDateString();
-    document.getElementById('select').style.display = 'block';
     let schedule: string[];
     const data = await this.sheetsservice.batchGetValues(this.appservice.getUserInfo().token.sheetId,
     'Schedule!A2:G2, Workout!C1:H5, StrengthEx!A1:C9');
@@ -112,5 +99,7 @@ export class WorkoutPage implements OnInit {
     this.strngthExData = data[2];
     const Day = new Date().getDay();
     this.weekDay = schedule[Day];
+    this.SelectedValue('Today');
+
     }
 }
